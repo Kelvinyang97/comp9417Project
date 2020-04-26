3aa7cf84c966ef4ba20c99adddaa7a18d98641cb
ascvetko <andrewcvetko@gmail.com>
2020-04-26 16:17:33 +1000

    Updated for different output types

--- output.py
+++ output.py
@@ -1,16 +1,14 @@
+import numpy as np
 from math import floor, ceil
 from preproc import preproc
 from sklearn.metrics import confusion_matrix
 from sklearn.linear_model import LogisticRegression
 
 
-def output_results(actual, predicted, probabilities, label_names, threshold = 0.0, 
-                   extra_output = True, test = True):
+def output_results(actual, predicted, probabilities, label_names, test = True):
     #Takes as inputs a numpy array of actual fitted labels, predited labels, 
-    #class probabilities, label names plus a float for prediction threshold,
-    #a boolean to print out extra output if wanted and a boolean if test data
-    #is being used. The output is printed to the terminal in the specified 
-    #table format.
+    #class probabilities, label names and a boolean if test data is being used. 
+    #The output is printed to the terminal in the specified table format.
     
     
     #Initialise variables
@@ -48,6 +46,77 @@ def output_results(actual, predicted, probabilities, label_names, threshold = 0.
     
     #Sort based on the highest to lowest values
     max_probs = sorted(max_probs, key = lambda x: x[0])[::-1]
+    
+    #Print results
+    #Print headers
+    print('=' * 93)
+    print('||  TOPIC NAME', ' ' * (40 - len('TOPIC NAME')), '||  PRECISION  ||    RECALL   ||      F1     ||')
+    print('=' * 93)
+    
+    #Loop through all topics
+    for i in range(len(label_names)):
+        
+        #Disregard irrelevant articles
+        if label_names[i] != 'IRRELEVANT':
+            
+            #Create formated variables to aid in printing
+            format_precision = '{:.2f}'.format(precision[i] * 100) + '%'
+            format_recall = '{:.2f}'.format(recall[i] * 100) + '%'
+            format_f1 = '{:.2f}'.format(f1[i] * 100) + '%'
+            len_fp = len(format_precision)
+            len_fr = len(format_recall)
+            len_ff = len(format_f1)
+            pre_fp = ceil((13 - len_fp) / 2)
+            post_fp = floor((13 - len_fp) / 2)
+            pre_fr = ceil((13 - len_fr) / 2)
+            post_fr = floor((13 - len_fr) / 2)
+            pre_ff = ceil((13 - len_ff) / 2)
+            post_ff = floor((13 - len_ff) / 2)
+
+            
+            #Print first line of output
+            print('||  ', label_names[i], ' ' * (42 - len(label_names[i])), 
+                  '||', ' ' * pre_fp, format_precision, ' ' * post_fp,
+                  '||', ' ' * pre_fr, format_recall, ' ' * post_fr,
+                  '||', ' ' * pre_ff, format_f1, ' ' * post_ff, '||', sep = '')
+            
+            print('=' * 93)
+
+    return
+
+
+
+def final_output_results(actual, predicted, probabilities, label_names, threshold = 0.0, 
+                   extra_output = True, test = True):
+    #Takes as inputs a numpy array of actual fitted labels, predited labels, 
+    #class probabilities, label names plus a float for prediction threshold,
+    #a boolean to print out extra output if wanted and a boolean if test data
+    #is being used. The output is printed to the terminal in the specified 
+    #table format.
+    
+    
+    #Initialise variables
+    precision = []
+    recall = []
+    f1 = []
+    max_probs = []
+    top_10_articles = [[] for i in range(len(label_names))]
+    matrix = np.zeros((len(label_names), len(label_names)))
+    
+    
+    #Determine recommended articles
+    #Calculate most likely prediction
+    for i in range(len(probabilities)):
+        
+        max_value = max(probabilities[i])
+        pred_class = probabilities[i].argmax()
+        if test:
+            max_probs.append([max_value, pred_class, i + 9501])
+        else:
+            max_probs.append([max_value, pred_class, i + 1])
+    
+    #Sort based on the highest to lowest values
+    max_probs = sorted(max_probs, key = lambda x: x[0])[::-1]
 
     #Get the 10 most relevant articles per topic
     for i in range(len(max_probs)):
@@ -56,6 +125,28 @@ def output_results(actual, predicted, probabilities, label_names, threshold = 0.
         if len(top_10_articles[topic]) < 10 and prob >= threshold:
             top_10_articles[topic].append(max_probs[i])
     print(top_10_articles)
+    
+    #Create confusion matrix
+    for i in range(len(top_10_articles)):
+            for j in range(len(top_10_articles[i])):
+                pred_class = top_10_articles[i][j][1]
+                if test:
+                    article_number = top_10_articles[i][j][2] - 9500
+                else:
+                    article_number = top_10_articles[i][j][2]
+                actual_class = actual[article_number - 1]
+                matrix[actual_class][i] = matrix[actual_class][i] + 1
+
+    
+    #Calculate precision, recall and f1 scores
+    for i in range(len(label_names)):
+        tp = matrix[i][i]
+        tpfp = sum(matrix[i])
+        tpfn = [sum(row[i] for row in matrix)][0]
+        precision.append(tp / tpfp)
+        recall.append(tp / tpfn)
+        f1.append((2 * tp / tpfp * tp / tpfn) / (tp / tpfp + tp / tpfn))
+    
     
     #Print results
     #Print headers
@@ -133,6 +224,7 @@ def output_results(actual, predicted, probabilities, label_names, threshold = 0.
     return
 
 
+
 # TRAINCSV = 'training.csv'
 # TESTCSV = 'test.csv'
 
@@ -152,6 +244,8 @@ def output_results(actual, predicted, probabilities, label_names, threshold = 0.
 # pred_probs = model1.predict_proba(train_features)
 
 # #Output results
-# output_results(train_labels, pred, pred_probs, list_of_labels, threshold = 0.8, 
+# final_output_results(train_labels, pred, pred_probs, list_of_labels, threshold = 0.0, 
 #                 extra_output = True, test = False)
 
+# output_results(train_labels, pred, pred_probs, list_of_labels, threshold = 0.8, 
+#                test = False)
